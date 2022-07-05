b2002e3497aafc29453c5c931981c6b18b3512f0
HenriqueMafort <hmafort@gmail.com>
2022-07-05 12:02:37 -0300

    iniciando projeto de menu

---
+++ Exercicio tabuada/script.js
@@ -0,0 +1,8 @@
+function calculo(){
+    var valortabuada = window.document.getElementById('txttab')
+    var res = window.document.getElementById('tab')
+    var v1 = Number(valortabuada.value)
+    if (valortabuada.value.length == 0){
+    alert('algo esta errado')
+    
+}
